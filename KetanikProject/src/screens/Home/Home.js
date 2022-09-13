eb913f7bf4ff06b41929d6157eafa0b610173c80
amir333 <a.mofid76@yahoo.com>
2022-09-13 15:24:52 +0430

    ss

--- KetanikProject/src/screens/Home/Home.js
+++ KetanikProject/src/screens/Home/Home.js
@@ -296,7 +296,7 @@ return (
           </TouchableOpacity>
           </View>
     </View>
-     {/* <ViewSlider
+     <ViewSlider
         renderSlides = {
           <>
             <TouchableOpacity onPress={()=>Linking.openURL(slider?.LinkSlider1)} style={styles(theme).viewBox}>
@@ -321,7 +321,7 @@ return (
       dotsContainerStyle={styles(theme).dotContainer}     // Container style of the pagination dots
       autoSlide = {true}    //The views will slide automatically
       slideInterval = {5000}    
-     />  */}
+     /> 
      
        <ScrollView >
   <View style={styles(theme).container}>
